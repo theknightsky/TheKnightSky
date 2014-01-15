5002e90c2a389417d3374ce1d471a35beb96f4b0
theknightsky <skyler.knight22@gmail.com>
2014-01-15 10:26:17 -0700

    Made view buttons responsive

--- javascripts/views/PortfolioItemListView.js
+++ javascripts/views/PortfolioItemListView.js
@@ -2,7 +2,7 @@ define(['backbone','handlebars','PortfolioItemView','PortfolioSlideView'], funct
 
 
 	var PortfolioItemListView = Backbone.View.extend({
-		el: '.page-outer',
+		el: '#page-outer',
 		events: {
 			'click #toList': 'renderList',
 			'click #toSlide': 'loadSlide',
@@ -12,8 +12,7 @@ define(['backbone','handlebars','PortfolioItemView','PortfolioSlideView'], funct
 			this.renderList();
 		},
 		renderList: function(){
-
-			this.$el.find('#portfolio-items-container').removeClass().empty();
+			this.$el.removeClass().addClass('list-active').find('#portfolio-items-container').removeClass().empty();
 			this.collection.each(function(model){
 				var portfolioItemView = new PortfolioItemView({model: model});
 				this.$el.find('#portfolio-items-container').append(portfolioItemView.el);
@@ -33,7 +32,7 @@ define(['backbone','handlebars','PortfolioItemView','PortfolioSlideView'], funct
 			}
 		},
 		renderSlide: function(slideNum){
-			this.$el.find('#portfolio-items-container').addClass('slide-container').empty();
+			this.$el.removeClass().addClass('slide-active').find('#portfolio-items-container').addClass('slide-container').empty();
 			// this.collection.each(function(model){
 				var portfolioSlideView = new PortfolioSlideView({model: this.collection.at(slideNum)});
 				this.$el.find('#portfolio-items-container').append(portfolioSlideView.el);

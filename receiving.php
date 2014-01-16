9fe57a25c069263116c7e31374fb4ffb138e4b4f
theknightsky <skyler.knight22@gmail.com>
2014-01-16 12:56:32 -0700

    initial contact form setup

---
+++ receiving.php
@@ -0,0 +1,36 @@
+<?php 
+ 
+    $name = $_POST['name']; 
+    $email = $_POST['email']; 
+    $message = $_POST['message']; 
+ 
+if(isset($_POST['submit']))
+ 
+{
+    $from_add = "contactform@theknightsky.com"; 
+ 
+    $to_add = "theknightsky22@gmail.com"; 
+     
+    $subject = "Your Subject Name";
+     
+    $message = "Name:$name \n Email: $email \n Message: $message";
+     
+    $headers = "From: $from_add \r\n";
+    $headers .= "Reply-To: $from_add \r\n";
+    $headers .= "Return-Path: $from_add\r\n";
+    $headers .= "X-Mailer: PHP \r\n";
+     
+     
+    if(mail($to_add,$subject,$message,$headers)) 
+    {
+        $msg = "Mail sent"
+    } 
+}
+     
+    print "&lt;p&gt;Thank you $name for your message, 
+we will be in contact shortly. &lt;a href=\"index.php\"&gt;Click here&lt;/a&gt; 
+to continue &lt;/p&gt;" ;
+     
+ 
+ 
+?>

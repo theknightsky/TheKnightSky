9a314b6c4be266bac067b820815f21783aea0476
theknightsky <skyler.knight22@gmail.com>
2014-01-16 15:53:44 -0700

    revise contact form =]

--- index.php
+++ index.php
@@ -4,17 +4,17 @@
     $message = $_POST['message'];
     $from = 'From: TheKnightSky.com'; 
     $to = 'theknightsky22@gmail.com'; 
-    $subject = 'Hello';
+    $subject = 'Message from TheKnightSky.com';
     $human = $_POST['human'];
             
-    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+    $body = "From: $name\n\nE-Mail: $email\n\nMessage:\n $message";
                 
     if ($_POST['submit'] && $human == '4') {                 
         if (mail ($to, $subject, $body, $from)) { 
         echo '<p>Your message has been sent!</p>';
-    } else { 
+        } else { 
         echo '<p>Something went wrong, go back and try again!</p>'; 
-    } 
+        } 
     } else if ($_POST['submit'] && $human != '4') {
     echo '<p>You answered the anti-spam question incorrectly!</p>';
     }
